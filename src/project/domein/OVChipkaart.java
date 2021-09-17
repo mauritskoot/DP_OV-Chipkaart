f4b8a0d99879a69ae2f98e4e872d4892d968032c
mauritskoot <43442846+mauritskoot@users.noreply.github.com>
2021-09-17 23:51:55 +0200

    klasse OVChipkaart toegevoegd

---
+++ src/project/domein/OVChipkaart.java
@@ -0,0 +1,11 @@
+package project.domein;
+
+import java.sql.Date;
+
+public class OVChipkaart {
+    private int kaartNummer;
+    private Date geldigTot;
+    private String klase;
+    private int saldo;
+    private int reizigerId;
+}
